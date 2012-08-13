e20eeb8073c47dd8abeb1383464568e40c0dac5f
Tim Heap <tim@ionata.com.au>
2012-08-13 15:16:34 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='django-bleach',
-    version="0.1.0",
+    version="0.1.1",
     description='Easily use bleach with Django models and templates',
     author='Tim Heap',
     author_email='heap.tim@gmail.com',
