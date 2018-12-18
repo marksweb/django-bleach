1b3a7f20681ff517d7e4915da0c5dd072b6861e7
Mark Walker <mark.walker@therealbuzzgroup.com>
2018-12-18 10:57:15 +0000

    Added git ignore file & changed author details

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='django-bleach',
     version="0.3.0",
     description='Easily use bleach with Django models and templates',
-    author='Tim Heap',
-    author_email='tim@timheap.me',
-    url='https://bitbucket.org/tim_heap/django-bleach',
+    author='Mark Walker',
+    author_email='theshow@gmail.com',
+    url='https://bitbucket.org/marksweb/django-bleach',
     packages=find_packages(),
     install_requires=[
         'bleach',
-        'Django>=1.3',
+        'Django>=1.8',
     ],
     package_data={},
     classifiers=[
