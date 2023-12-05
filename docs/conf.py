d0fa6a2f86dbdf6fcf1bb518f97be26a15590b28
Mark Walker <theshow@gmail.com>
2023-12-05 02:04:24 +0000

    docs: Change to furo (#114)

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,15 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+try:
+    import furo
+
+    html_theme = "furo"
+    html_theme_options = {
+        "navigation_with_keys": True,
+    }
+except ImportError:
+    html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
